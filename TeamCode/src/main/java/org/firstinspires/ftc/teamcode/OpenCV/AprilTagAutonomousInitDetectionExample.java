ac3e2b91dfbf4bdc57df8a133b1625c4f1eafc16
jiaxi <saltycupofsoda@gmail.com>
2022-12-03 12:55:24 -0800

    added old programs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/AprilTagAutonomousInitDetectionExample.java
@@ -0,0 +1,309 @@
+/*
+ * Copyright (c) 2021 OpenFTC Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+import java.util.ArrayList;
+
+@Autonomous
+public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
+{
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    //Tag IDs of cone
+    int onedot = 1;
+    int twodot = 2;
+    int threedot = 3;
+
+    AprilTagDetection tagOfInterest = null;
+
+    //motor
+    DcMotor fl;
+    DcMotor bl;
+    DcMotor fr;
+    DcMotor br;
+
+    //servo
+    private Servo claw_servo;
+    private Servo right_arm;
+    private Servo left_arm;
+
+    public void initialize() {
+
+        //motors
+        fl = hardwareMap.dcMotor.get("FL");
+        bl = hardwareMap.dcMotor.get("BL");
+        fr = hardwareMap.dcMotor.get("FR");
+        br = hardwareMap.dcMotor.get("BR");
+
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.FORWARD);
+        fr.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // set up the two arm servos
+        right_arm = hardwareMap.get(Servo.class, "rServo");
+        left_arm = hardwareMap.get(Servo.class, "lServo");
+
+        // set the arm position to the correct positions
+        right_arm.setPosition(0.9);
+        left_arm.setPosition(0.1);
+
+        claw_servo = hardwareMap.get(Servo.class, "cServo");
+        claw_servo.setDirection(Servo.Direction.REVERSE);
+        claw_servo.setPosition(0);
+
+        //odometry
+
+//            fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+    @Override
+    public void runOpMode()
+    {
+        initialize();
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == onedot || tag.id == twodot || tag.id == threedot)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        /* Actually do something useful */
+        if(tagOfInterest == null || tagOfInterest.id == twodot)
+        {
+            arm();
+            sleep(secondsToMilli(1));
+            timedTranslate(1, 0.5, 0);
+            timedTranslate(1, 0.5, 0);
+            timedTranslate(1, -0.5, 0);
+            right_arm.setPosition(0.9);
+            left_arm.setPosition(0.1);
+        } else if (tagOfInterest.id == onedot) {
+            arm();
+            sleep(secondsToMilli(1));
+            timedTranslate(1, 0.6, 2);
+            sleep(secondsToMilli(1));
+            timedTranslate(1, 0.5, 0);
+            right_arm.setPosition(0.9);
+            left_arm.setPosition(0.1);
+        } else if (tagOfInterest.id == threedot) {
+            arm();
+            sleep(secondsToMilli(1));
+            timedTranslate(1, 0.6, 1);
+            sleep(secondsToMilli(1));
+            timedTranslate(1, 0.5, 0);
+            right_arm.setPosition(0.9);
+            left_arm.setPosition(0.1);
+        }
+
+
+        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
+        while (opModeIsActive()) {sleep(20);}
+    }
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+
+    public int secondsToMilli(int seconds) {
+        int milliseconds = 1000 * seconds;
+        return milliseconds;
+    }
+
+    public void arm() {
+        right_arm.setPosition(0.1);
+        left_arm.setPosition(0.9);
+    }
+
+    public void timedTranslate(int seconds, double power, int direction) {
+        double flPow = power;
+        double blPow = power;
+        double frPow = power;
+        double brPow = power;
+        if (direction == 1) {
+            blPow = -power;
+            frPow = -power;
+        } else if (direction == 2) {
+            flPow = -power;
+            brPow = -power;
+        }
+
+        fl.setPower(flPow);
+        fr.setPower(frPow);
+        bl.setPower(blPow);
+        br.setPower(brPow);
+
+        sleep(secondsToMilli(seconds));
+        motorStop();
+    }
+
+    public void motorStop() {
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlayMechanisms.java
@@ -0,0 +1,247 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.CursorLoader;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp (name= "Draft2PowerPlay")
+public class PowerPlayMechanisms extends OpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // VARIABLES FOR THE CLAW
+    private Servo claw_servo;
+
+    // VARIABLES FOR THE FOUR MOTORS
+    private DcMotor fl = null;
+    private DcMotor fr = null;
+    private DcMotor bl = null;
+    private DcMotor br = null;
+
+    // VARIABLES FOR THE LIFT (ALWAYS INT)
+    private DcMotor rightLiftMotor;
+    private DcMotor leftLiftMotor;
+    private Servo right_arm;
+    private Servo left_arm;
+
+    private int  rightLiftPosition;
+    private int leftLiftPosition;
+    private int liftIncrement = 100;
+    boolean flag = true;
+
+//    private double rArmPosition; <-- irrelevant
+//    private double lArmPosition; <-- irrelevant
+//    private double aIncrement = 0.05; <-- irrelevant
+
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        //SET UP THE CLAW
+        claw_servo = hardwareMap.get(Servo.class, "cServo");
+        claw_servo.setDirection(Servo.Direction.REVERSE);
+        claw_servo.setPosition(0);
+        telemetry.addData("Motors", "right (%.2f)", claw_servo.getPosition());
+
+        //SET UP THE MOTORS FOR THE DRIVE TRAIN
+        fl = hardwareMap.get(DcMotor.class, "FL");
+        fr = hardwareMap.get(DcMotor.class, "FR");
+        bl = hardwareMap.get(DcMotor.class, "BL");
+        br = hardwareMap.get(DcMotor.class, "BR");
+
+        //SET THE DIRECTIONS FOR THE MOTORS
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        fr.setDirection(DcMotorSimple.Direction.FORWARD);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // set up the two lift motors
+        rightLiftMotor = hardwareMap.get(DcMotor.class, "rlMotor");
+        leftLiftMotor = hardwareMap.get(DcMotor.class, "llMotor");
+
+        // set up the two arm servos
+        right_arm = hardwareMap.get(Servo.class, "rServo");
+        left_arm = hardwareMap.get(Servo.class, "lServo");
+
+        // set the arm position to the correct positions
+        right_arm.setPosition(0.75);
+        left_arm.setPosition(0.25);
+
+
+        // set the direction of the motors
+        rightLiftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftLiftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // set the motors to run with encoder
+        rightLiftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        leftLiftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        // set target to zero
+        rightLiftMotor.setTargetPosition(0);
+        leftLiftMotor.setTargetPosition(0);
+
+        // stop and reset the encoders
+        rightLiftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftLiftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // stop and reset the encoders
+        rightLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        rightLiftMotor.setPower(0.9);
+        leftLiftMotor.setPower(0.9);
+
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+
+        // SETTING THE VARIABLES FOR THE FOUR MOTORS
+        double frontLeftPower;
+        double frontRightPower;
+        double backLeftPower;
+        double backRightPower;
+
+        double drive_y = -gamepad1.left_stick_y;
+        double drive_x = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x; // random comment
+
+        if(gamepad1.right_trigger == 0){
+            frontLeftPower =   Range.clip(drive_y + drive_x + turn, -0.5, 0.5);
+            frontRightPower = Range.clip(drive_y - drive_x - turn, -0.5, 0.5);
+            backLeftPower =  Range.clip(drive_y - drive_x + turn, -0.5,0.5);
+            backRightPower =   Range.clip(drive_y + drive_x - turn, -0.5, 0.5);
+
+        } else {
+            frontLeftPower =  Range.clip(drive_y + drive_x + turn, -0.7, 0.7);
+            frontRightPower =  Range.clip(drive_y - drive_x - turn, -0.7, 0.7);
+            backLeftPower =  Range.clip(drive_y - drive_x + turn, -0.7,0.7);
+            backRightPower =  Range.clip(drive_y + drive_x - turn, -0.7, 0.7);
+        }
+
+        fl.setPower(frontLeftPower);
+        fr.setPower(frontRightPower);
+        bl.setPower(backLeftPower);
+        br.setPower(backRightPower);
+
+
+
+        // CODE FOR THE CLAW
+        if (gamepad2.x){                            // open claw
+            claw_servo.setPosition(0.2);
+
+        } else if (gamepad2.b) {
+            claw_servo.setPosition(0);
+        }
+
+        // CODE FOR THE LIFT
+        // 3.138 : 1 ratio old to new junction position
+        // set up the lift motors to move to the set positions at the same time
+        if (gamepad2.dpad_up){ // high junction
+            rightLiftMotor.setTargetPosition(1625);
+            leftLiftMotor.setTargetPosition(1625);
+        } else if(gamepad2.dpad_left){
+            rightLiftMotor.setTargetPosition(825);
+            leftLiftMotor.setTargetPosition(824);
+        } else if(gamepad2.dpad_down){
+            rightLiftMotor.setTargetPosition(0);
+            leftLiftMotor.setTargetPosition(0);
+        }
+
+//        if (gamepad2.left_stick_button){ // going up
+//            if(flag){
+//                rArmPosition = right_arm.getPosition();
+//                lArmPosition = left_arm.getPosition();
+//                right_arm.setPosition(rArmPosition -= increment);
+//                left_arm.setPosition(lArmPosition += increment);
+//                flag = false;
+//            }
+//
+//        } else if (gamepad2.right_stick_button) {
+//            if (flag) {
+//                rArmPosition = right_arm.getPosition();
+//                lArmPosition = left_arm.getPosition();
+//                right_arm.setPosition(rArmPosition+=increment);
+//                left_arm.setPosition(lArmPosition-=increment);
+//                flag = false;
+//            }
+//        } else {
+//            flag = true;
+//        }
+        // TOGGLE THE LIFT CODE
+        if(gamepad2.left_trigger > 0.05){ // lift goes up
+            if(flag){
+                rightLiftPosition = rightLiftMotor.getCurrentPosition();
+                leftLiftPosition = leftLiftMotor.getCurrentPosition();
+                rightLiftMotor.setTargetPosition(rightLiftPosition += liftIncrement);
+                leftLiftMotor.setTargetPosition(leftLiftPosition += liftIncrement);
+                flag = false;
+
+            }
+        } else if (gamepad2.right_trigger > 0.05){ // lift goes down
+            if (flag) {
+                rightLiftPosition = rightLiftMotor.getCurrentPosition();
+                leftLiftPosition = leftLiftMotor.getCurrentPosition();
+                rightLiftMotor.setTargetPosition(rightLiftPosition -= liftIncrement); // this says --> rightliftposition = rightliftposition  - liftincrement
+                leftLiftMotor.setTargetPosition(leftLiftPosition -= liftIncrement);
+                flag = false;
+            }
+
+        } else {
+            flag = true;
+        }
+
+
+        // CODE FOR LIFTING AND DROPPING THE ARMS
+        if (gamepad2.y){
+            right_arm.setPosition(0.35);
+            left_arm.setPosition(0.65);
+
+        } else if (gamepad2.a){
+            right_arm.setPosition(0.75); // closer to one, means lower
+            left_arm.setPosition(0.25); // closer to zero, means lower
+
+        }
+
+        //CODE TO SCORE ON THE GROUND JUNCTION
+        if (gamepad2.dpad_right){
+            right_arm.setPosition(0.7);
+            left_arm.setPosition(0.3);
+        }
+
+        // set the right arm to 0.6 for the low junction
+        // set the right from 1 to 0.6
+
+        // the left is going to go from zero to 0.4
+
+
+
+
+
+        telemetry.addData("Status", "Rune Time: " + runtime.toString() );
+        telemetry.addData("Encoder count Right: ", + rightLiftMotor.getCurrentPosition());
+        telemetry.addData("Encoder Counts Left: ", + leftLiftMotor.getCurrentPosition());
+        telemetry.addData("Motors", "front left (%.2f), front right (%.2f), back left (%.2f), back right (%.2f)", frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+        telemetry.addData("Gamepad coordinates","(x,y) = (%.2f, %.2f), turn = %.2f", drive_x, drive_y, turn);
+        telemetry.addData("Servo Position", "Claw position: (%5.2f)" , claw_servo.getPosition());
+        telemetry.addData("Servo", "right arm:  (%.2f)", right_arm.getPosition());
+        telemetry.addData("Servo", "left arm: (%.2f)", left_arm.getPosition());
+
+
+
+        telemetry.update();
+
+
+
+    }
+}
